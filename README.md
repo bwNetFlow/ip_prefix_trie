fd0bba918273db778aa3873dfa18e1fa8a90ad79
Daniel Nägele <code@danieln.de>
2018-04-13 15:44:43 +0200

    improve docs

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
+## IP Prefix Trie
+This module is intended for matching IP addresses to arbitrary prefix networks.
+This is done using a classic prefix tree (or trie) which stores data at all
+nodes within an existing IP network prefix. This module supports both, IPv6 and
+legacy IPv4. Separate trees have to be kept for each protocol, of course. See
+the following example using an integer, which might represent a customer ID:
+
+```
+v6_trie.Insert(123, []string{"2001:db8:1234::/48"})
+
+ip, _, _ := net.ParseCIDR("2001:db8:1234::7/64")
+matched_id := v6_trie.Lookup(ip)
+```
+
+More examples can be seen in the test cases. This is currently in use to match
+Netflow data to customer IDs which are used for billing on a specific upstream
+link.
+
+
+## Internal workings
+This module implements a standard trie, which operates on 0 and 1 for
+branching. Some care is taken to implement longest prefix matching (i.e.
+matching the most specific network) correctly. The payload contents are stored
+at every node (see Todo section) and are corrected when inserting another,
+longer (or equal) prefix. The code is thoroughly commented, go take a look.
+
+Wikipedia has some more [details](https://en.wikipedia.org/wiki/Trie) on tries
+in general.
+
 
 ## Benchmark Results
+Benchmark results from my Laptop, so you don't have to run them yourself:
 
 ```
 > go test -bench=. -test.benchtime=10s
@@ -13,3 +43,12 @@ BenchmarkIPv6LookupPropableMiss-8   	100000000	       107 ns/op
 PASS
 ok  	ip_prefix_trie	68.719s
 ```
+
+In a real environment looking up customer IDs, you can expect lookups within
+1us (99th percentile) at about 10k lookups per second using two trees (v6 and
+v4) simultaneously. They'll take 600ns on average. Perhaps above benchmark
+setup is not very realistic.
+
+## TODO
+ * implement payloads as pointer to reduce memory footprint
+ * do more realistic benchmarking (and testing)
--- ip_prefix_trie.go
+++ ip_prefix_trie.go
@@ -6,6 +6,7 @@ import (
 	"net"
 )
 
+// this is the fundamental type, a classic binary tree struct
 type TrieNode struct {
 	LNode, RNode *TrieNode
 	Payload      interface{}
@@ -30,17 +31,22 @@ func (u Uint128) ShiftRight(n uint) Uint128 {
 
 // used to fill our custom type, basically the same as FromBytes
 func ip2int(ip net.IP) Uint128 {
+	// the .To16() bit is important, net.IP sometimes stores v4 addresses
+	// in smaller slices instead of using large ones with padding
 	hi := binary.BigEndian.Uint64(ip.To16()[:8])
 	lo := binary.BigEndian.Uint64(ip.To16()[8:])
 	return Uint128{hi, lo}
 }
 
-// Insert data for a number of prefixes below a given root node
+// Insert data for a number of prefixes below a given root node.
 //
 // Parameters:
 //	- trie is a (root) TrieNode
 //	- payload is the data stored for a prefix
 //	- prefixes is a slice of prefixes
+//
+// Note that this could be done recursively, too. The use case however does not
+// include non-root inserts or lookups.
 func (root *TrieNode) Insert(payload interface{}, prefixes []string) {
 	for _, cidr := range prefixes {
 		current_node := root
@@ -50,6 +56,7 @@ func (root *TrieNode) Insert(payload interface{}, prefixes []string) {
 		}
 		plen, max_plen := prefix.Mask.Size()
 		var bits Uint128 = ip2int(ip)
+		// Iterate to the correct node under which we insert.
 		for i := 0; i <= plen; i++ {
 			next_bit := bits.ShiftRight(uint(max_plen-i)).L & 1
 			var next_node **TrieNode
@@ -58,6 +65,7 @@ func (root *TrieNode) Insert(payload interface{}, prefixes []string) {
 			} else if next_bit == 1 {
 				next_node = &current_node.RNode
 			}
+			// Insert.
 			if *next_node == nil {
 				*next_node = new(TrieNode)
 				(*next_node).Payload = current_node.Payload
@@ -65,7 +73,7 @@ func (root *TrieNode) Insert(payload interface{}, prefixes []string) {
 			current_node = *next_node
 		}
 		current_node.Payload = payload        // needed, might be set by less specific
-		current_node.set_for_subtrie(payload) // overwrites all empty nodes below this
+		current_node.set_for_subtrie(payload) // overwrites empty nodes below (see func)
 	}
 }
 
@@ -75,15 +83,16 @@ func (root *TrieNode) Insert(payload interface{}, prefixes []string) {
 //	2. A was added before B
 //	3. B needs to match more specific prefixes which are not in A
 //	4. TrieNodes below B exist as a connection to A, and may be
-//	   uninitialised if the differnce between both prefix lengths is > 1
+//	   uninitialised (i.e. wth payload nil) if the differnce between both
+//	   prefix lengths is > 1
 //	5. If such empty TrieNodes exist, they need to be updated to B's
-//	   content
+//	   content, such that all addressis within B come back as a match
 func (node *TrieNode) set_for_subtrie(payload interface{}) {
 	if node != nil {
-		// this check prevents more specific prefixes which already
-		// exist in the trie from being overwritten, if they're
-		// initialised
-		// Second branch in OR is needed to call recursively.
+		// The following check prevents more specific prefixes which
+		// already exist in the trie from being overwritten, if they're
+		// initialised.
+		// The second part of the OR is needed on the first call.
 		if node.Payload == nil || node.Payload == payload {
 			node.Payload = payload
 			node.LNode.set_for_subtrie(payload)
@@ -94,6 +103,7 @@ func (node *TrieNode) set_for_subtrie(payload interface{}) {
 
 // Match an IP to the prefixes and return the correct content.
 // Selects IPv4 or IPv6 mode, i.e. the correct prefix length, automatically.
+// The tree however needs to be built with the correct addresses.
 func (root *TrieNode) Lookup(ip net.IP) interface{} {
 	current_node := root
 
@@ -108,6 +118,7 @@ func (root *TrieNode) Lookup(ip net.IP) interface{} {
 	}
 
 	var bits Uint128 = ip2int(ip)
+	// Lookup correct Trie Node iteratively.
 	for i := uint(max_plen); i > 0; i-- {
 		next_bit := bits.ShiftRight(i).L & 1
 		if next_bit == 0 && current_node.LNode != nil {
